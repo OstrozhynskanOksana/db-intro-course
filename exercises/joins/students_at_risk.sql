f289a994865a8c40ecc9c3e761b167a017e7ec00
Oksana-dev <ostroginskaa@gmail.com>
2026-05-11 17:22:29 +0300

    Implement query to find students at risk

--- exercises/joins/students_at_risk.sql
+++ exercises/joins/students_at_risk.sql
@@ -12,3 +12,18 @@
 --          - оцінкою (зростання), потім за назвою групи, потім за іменем студента, потім за назвою курсу
 
 -- Рішення:
+select ps.first_name ||' '|| ps.last_name as student_name, 
+g.name as group_name, 
+c.name as course_name, 
+e.grade,
+pp.first_name ||' '|| pp.last_name as lecturer_name 
+from student s
+join person ps on s.person_id = ps.person_id 
+join student_group g on g.group_id = s.group_id
+join enrolment e on e.student_id = s.student_id
+join course c on e.course_id = c.course_id 
+join course_teacher ct on c.course_id = ct.course_id 
+join professor p2 on ct.professor_id = p2.professor_id
+join person pp on p2.person_id  = pp.person_id
+where e.grade < 60 and ct.professor_role = 'лектор'
+order by e.grade, group_name, student_name, course_name
