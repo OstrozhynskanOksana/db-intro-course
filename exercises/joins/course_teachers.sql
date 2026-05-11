67e8f949a946fdbfbc371ed31380a08a673fd5fc
Oksana-dev <ostroginskaa@gmail.com>
2026-05-11 15:06:58 +0300

    Implement SQL query for course teachers

--- exercises/joins/course_teachers.sql
+++ exercises/joins/course_teachers.sql
@@ -9,3 +9,7 @@
 --          - назвою курсу, потім за роллю викладача
 
 -- Рішення:
+select c.name as course_name, p.first_name ||' '|| p.last_name as professor_name, t.professor_role from person p 
+join professor p2 on p.person_id = p2.person_id
+join course_teacher t on t.professor_id = p2.professor_id 
+join course c on c.course_id = t.course_id 
