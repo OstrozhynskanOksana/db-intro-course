10aa32d6c90d0cf8127fc483b1c56a1ed1f4c076
Oksana-dev <ostroginskaa@gmail.com>
2026-05-11 16:22:22 +0300

    Implement query for professors not serving as curators

--- exercises/joins/professor_not_curator.sql
+++ exercises/joins/professor_not_curator.sql
@@ -7,3 +7,8 @@
 --          - повним іменем викладача
 
 -- Рішення:
+select p.first_name ||' '|| p.last_name as professor_name, p2.job from person p 
+join professor p2 on p.person_id = p2.person_id
+where p2.professor_id not in (select p3.professor_id from professor p3
+join student_group g on p3.professor_id = g.curator_id) and p2.status = 'викладає'
+order by professor_name
