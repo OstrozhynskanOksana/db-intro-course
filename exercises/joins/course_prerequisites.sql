6772f7fd4efb7b230404c93a709c6dfe3745c11a
Oksana-dev <ostroginskaa@gmail.com>
2026-05-11 14:39:58 +0300

    Add SQL query for course prerequisites with sorting

--- exercises/joins/course_prerequisites.sql
+++ exercises/joins/course_prerequisites.sql
@@ -8,3 +8,9 @@
 --          - назвою курсу, потім за назвою пре-реквізиту
 
 -- Рішення:
+select c.name as course_name, p.name as prerequisite_name from course c 
+left join course_prerequisite cp on cp.course_id = c.course_id 
+left join course p on cp.prerequisite_course_id  = p.course_id 
+order by course_name, prerequisite_name 
+
+
