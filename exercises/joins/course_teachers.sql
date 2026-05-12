ac94c9dc28df6f5a3262700041fc65d1182a2cbf
Oksana-dev <ostroginskaa@gmail.com>
2026-05-12 13:08:52 +0300

    Fix SQL query to use 'teacher_name' and alias 'role'

--- exercises/joins/course_teachers.sql
+++ exercises/joins/course_teachers.sql
@@ -9,9 +9,9 @@
 --          - назвою курсу, потім за роллю викладача
 
 -- Рішення:
-select c.name as course_name, p.first_name ||' '|| p.last_name as professor_name, t.professor_role from person p 
+select c.name as course_name, p.first_name ||' '|| p.last_name as teacher_name, t.professor_role as role from person p 
 join professor p2 on p.person_id = p2.person_id
 join course_teacher t on t.professor_id = p2.professor_id 
 join course c on c.course_id = t.course_id 
 where c.status = 'активний'
-order by course_name, t.professor_role 
+order by course_name, role 
