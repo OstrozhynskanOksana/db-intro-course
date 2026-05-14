3469ec5d079319dc561cf06a35202ff5588dacdd
Oksana-dev <ostroginskaa@gmail.com>
2026-05-14 17:16:21 +0300

    Refactor university activities SQL query

--- exercises/joins/university_activities.sql
+++ exercises/joins/university_activities.sql
@@ -11,28 +11,25 @@
 --          - назвою курсу, потім за типом активності, потім за іменем
 
 -- Рішення:
-select *
-from (
-    select 
-        ps.first_name || ' ' || ps.last_name as full_name,
-        c.name as course_name,
-        'запис на курс' as activity_type
-    from student s
-    join person ps on s.person_id = ps.person_id 
-    join enrolment e on s.student_id = e.student_id 
-    join course c on e.course_id = c.course_id
-    where c.status = 'активний'
+ select 
+     ps.first_name || ' ' || ps.last_name as full_name,
+     c.name as course_name,
+     'запис на курс' as activity_type
+ from student s
+ join person ps on s.person_id = ps.person_id 
+ join enrolment e on s.student_id = e.student_id 
+ join course c on e.course_id = c.course_id
+ where c.status = 'активний'
       
-    union 
+ union 
 
-    select
-        pp.first_name || ' ' || pp.last_name as full_name,
-        c.name as course_name,
-        'викладання курсу' as activity_type 
-    from professor p 
-    join person pp on p.person_id = pp.person_id 
-    join course_teacher ct on p.professor_id = ct.professor_id 
-    join course c on ct.course_id = c.course_id 
-    where c.status = 'активний'
-) as activities
-order by course_name asc, activity_type asc, full_name collate "uk-UA-x-icu" asc;
+ select
+     pp.first_name || ' ' || pp.last_name as full_name,
+     c.name as course_name,
+     'викладання курсу' as activity_type 
+ from professor p 
+ join person pp on p.person_id = pp.person_id 
+ join course_teacher ct on p.professor_id = ct.professor_id 
+ join course c on ct.course_id = c.course_id 
+ where c.status = 'активний'
+order by course_name asc, activity_type asc, full_name asc;
